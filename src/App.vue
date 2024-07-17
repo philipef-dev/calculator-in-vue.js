9f3a376195db46940603b86ad49d63c8f921864d
PhilipeF <philipesferreiraa@gmail.com>
2024-07-16 22:09:52 -0300

    Feat: Projeto - ok

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,42 @@
 <script setup>
+import { reactive } from "vue";
 
+const values = reactive({
+  firstNumber: 0,
+  secondNumber: 0,
+  operacao: "adicao",
+  result: 0,
+});
 
+function calcular() {
+  switch (values.operacao) {
+    case "adicao":
+      values.result = values.firstNumber + values.secondNumber;
+      break;
+    case "subtracao":
+      values.result = values.firstNumber - values.secondNumber;
+      break;
+    case "multiplicacao":
+      values.result = values.firstNumber * values.secondNumber;
+      break;
+    case "divisao":
+      values.result = values.firstNumber / values.secondNumber;
+      break;
+  }
+}
+
+function handleInputChange(inputType, event) {
+  const value = event.target.value === "" ? 0 : parseFloat(event.target.value);
+
+  if (inputType === "firstNumber") {
+    values.firstNumber = value;
+  } else if (inputType === "secondNumber") {
+    values.secondNumber = value;
+  } else if (inputType === "operacao") {
+    values.operacao = event.target.value;
+  }
+  calcular();
+}
 </script>
 
 <template>
@@ -12,14 +48,19 @@
       <input
         type="number"
         class="w-25 form-control"
+        @input="(e) => handleInputChange('firstNumber', e)"
         placeholder="Digite um numero"
       />
       <input
         type="number"
         class="w-25 form-control"
+        @input="(e) => handleInputChange('secondNumber', e)"
         placeholder="Digite um numero"
       />
-      <select class="w-25 form-control">
+      <select
+        class="w-25 form-control"
+        @change="(e) => handleInputChange('operacao', e)"
+      >
         <option value="adicao">Adição</option>
         <option value="subtracao">Subtração</option>
         <option value="multiplicacao">Multiplicação</option>
@@ -27,7 +68,10 @@
       </select>
     </div>
     <div class="d-flex justify-content-center mt-4">
-
+      <div class="text-center border border-dark rounded p-5">
+        <h1>Resultado</h1>
+        <h1 class="display-1">{{ values.result }}</h1>
+      </div>
     </div>
   </div>
 </template>
