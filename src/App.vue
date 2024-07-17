4e67ecbe49a3f29620bfbc0128980a28dd6bc341
PhilipeF <philipesferreiraa@gmail.com>
2024-07-16 21:11:29 -0300

    Feat: Estrutura HTML/CSS da aplicação 80% - ok

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script setup>
+
+
 </script>
 
 <template>
-    
+  <div class="container">
+    <div class="text-center mt-5">
+      <h1>Calculadora com Vue.js</h1>
+    </div>
+    <div class="d-flex flex-column mt-4 align-items-center gap-3">
+      <input
+        type="number"
+        class="w-25 form-control"
+        placeholder="Digite um numero"
+      />
+      <input
+        type="number"
+        class="w-25 form-control"
+        placeholder="Digite um numero"
+      />
+      <select class="w-25 form-control">
+        <option value="adicao">Adição</option>
+        <option value="subtracao">Subtração</option>
+        <option value="multiplicacao">Multiplicação</option>
+        <option value="divisao">Divisão</option>
+      </select>
+    </div>
+    <div class="d-flex justify-content-center mt-4">
+
+    </div>
+  </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
